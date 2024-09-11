e861507f2557bf5a2ed23d9750ddc43c3966956b
Shekhar <108127963+ShekharPareek@users.noreply.github.com>
2024-09-11 05:56:31 +0000

    code udate

--- dist/assets/tr-d5c962f0.js
+++
@@ -1,1 +0,0 @@
-Intl.PluralRules&&typeof Intl.PluralRules.__addLocaleData=="function"&&Intl.PluralRules.__addLocaleData({data:{categories:{cardinal:["one","other"],ordinal:["other"]},fn:function(a,l){return l?"other":a==1?"one":"other"}},locale:"tr"});
--- dist/assets/tslib.es6-bcad6c82.js
+++
@@ -1,1 +0,0 @@
-import{g as e,t as s}from"./index-b03c14eb.js";const r=e(s);export{r};
