6e05392a49aa0387857980dacecda69b18729b7c
Shekhar <108127963+ShekharPareek@users.noreply.github.com>
2024-09-11 05:41:26 +0000

    code udate

--- dist/assets/ja-abb1af27.js
+++
@@ -1,1 +0,0 @@
-Intl.PluralRules&&typeof Intl.PluralRules.__addLocaleData=="function"&&Intl.PluralRules.__addLocaleData({data:{categories:{cardinal:["other"],ordinal:["other"]},fn:function(a,l){return"other"}},locale:"ja"});
