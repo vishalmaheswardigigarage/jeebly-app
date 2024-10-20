199c8a4042aaf7993322c43bfa78370c05c74e21
ShekharPareek <shekhar.pareek@sunarctechnologies.com>
2024-10-20 13:51:56 +0530

    code push

--- index.js
+++ index.js
@@ -597,7 +597,7 @@ async function createShipment({
     description: description,
     weight: weight || "1000",
     payment_type: paymentType,
-    cod_amount: "0",
+    cod_amount: codAmount||"0.00",
     num_pieces: pieces,
     customer_reference_number: orderNumber || "#001",
     origin_address_name: defaultAddress.addr_area,
