5a7c80e99be69328677d4a0fbe7599a789db3de7
ShekharPareek <shekhar.pareek@sunarctechnologies.com>
2024-10-24 13:52:54 +0530

    code repush

--- shopify.js
+++ shopify.js
@@ -5,11 +5,18 @@ import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlit
 import { restResources } from "@shopify/shopify-api/rest/admin/2024-07";
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';  // Import for ES module __dirname-like functionality
 import fs from 'fs';
 
 
 dotenv.config();  // Load environment variables
 
+
+// Manually create __dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Ensure the directory for SQLite database exists
 const dbDirectory = path.join(__dirname, 'database');
 if (!fs.existsSync(dbDirectory)) {
   fs.mkdirSync(dbDirectory);  // Create the directory if it doesn't exist
@@ -17,7 +24,7 @@ if (!fs.existsSync(dbDirectory)) {
 
 // Define the full path to the SQLite database file
 const dbPath = path.join(dbDirectory, 'sessions.db');
-const sessionDb = new SQLiteSessionStorage(dbPath);  // Use SQLite session storag
+const sessionDb = new SQLiteSessionStorage(dbPath);  // Use SQLite session storage
 
 const shopify = shopifyApp({
   api: {
